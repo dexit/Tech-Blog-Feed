a23e4d4ca00ef9a7eca72d55e8081370100f3baf
dExIT <dexit@dyc.lv>
2023-07-10 18:40:26 +0100

    udpated serverjs

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-/* These lines of code are importing necessary modules and dependencies for the application. */
 const express = require("express");
 const path = require("path");
 const sequelize = require("./config/connection");
@@ -8,13 +7,10 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const helpers = require('./utils/helpers')
 
 const routes = require("./controllers");
-// const helpers = require("./util/helpers");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-/* `const hbs = exphbs.create({ helpers });` creates a new instance of the Handlebars engine with the
-`helpers` object passed as an argument. This allows the application to use custom helper functions
-in the Handlebars templates. */
 const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "Super secret secret",
@@ -25,7 +21,7 @@ const sess = {
     db: sequelize,
   }),
 };
-/* These lines of code are setting up and configuring the Express application. */
+
 app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -35,7 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
