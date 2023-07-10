ea011fb285183f4b087d5f4b85d57b4c51e590b5
dExIT <dexit@dyc.lv>
2023-07-10 18:49:50 +0100

    updated as could not launch on heroku

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,23 +1,23 @@
 const sequelize = require("../config/connection");
 const { User, Blog, Comment } = require("../models");
 
-const users = require("./users.json");
-const blogs = require("./blogs.json");
-const comments = require("./comments.json");
+const userData = require("./userData.json");
+const blogData = require("./blogData.json");
+const commentData = require("./commentData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  const users = await User.bulkCreate(users, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
-  for (var blog of blogs) {
+  for (var blog of blogData) {
     await Blog.create({
       ...blog,
       user_id: users[Math.floor(Math.random() * users.length)].id
     });
   }
-    for (const comment of comments) {
+    for (const comment of commentData) {
       await Comment.create({
       ...comment,
         user_id: users[Math.floor(Math.random() * users.length)].id,
