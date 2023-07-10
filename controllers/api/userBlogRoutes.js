960647690247e95846ff3762480673a37112e71f
dExIT <dexit@dyc.lv>
2023-07-10 17:24:58 +0100

    Setup userBlogRoutes

--- controllers/api/userBlogRoutes.js
+++ controllers/api/userBlogRoutes.js
@@ -1,6 +1,34 @@
 const router = require("express").Router();
 const { Blog , Comment } = require("../../models");
 const withAuth = require("../../utils/withAuth");
+router.get("/", async (req, res) => {
+    try {
+      const blogData = await Blog.findAll({
+        attributes: ["id", "title", "content", "date"],
+        order: [["date", "ASC"]],
+        include: {
+          model: Comment
+        }
+      });
+      res.status(200).json(blogData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+  router.get("/:id", async (req, res) => {
+    try {
+      const blogData = await Blog.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json(blogData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });  
 
 router.post("/", withAuth, async (req, res) => {
     try {
@@ -11,5 +39,34 @@ router.post("/", withAuth, async (req, res) => {
       res.status(200).json(newBlog);
     } catch (error) {
       res.status(400).json(error);
+    }
+  });
+  router.put("/:id", withAuth, async (req, res) => {
+    try {
+      const newBlog = await Blog.update(req.body,{
+        where: {
+          id: req.params.id,
+        }})
+      res.status(200).json(newBlog);
+    } catch (error) {
+      console.error(error)
+      res.status(400).json(error);
+      console.error(error)
+    }
+  });
+  router.delete("/:id", withAuth, async (req, res) => {
+    try {
+      const blogData = await Blog.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!blogData) {
+        res.status(404).json({ message: "No blog found with this id!" });
+        return;
+      }
+      res.status(200).json(blogData);
+    } catch (error) {
+      res.status(500).json(error);
     }
   });
