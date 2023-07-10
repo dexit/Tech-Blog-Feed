2bbe6c29397244dce2325304d71bf0d0dae8bbcc
dExIT <dexit@dyc.lv>
2023-07-10 17:50:16 +0100

    Added missing HBS templ added logic and layouts

---
+++ public/js/delete.js
@@ -0,0 +1,14 @@
+const deleteHandler = async (event) => {
+    event.preventDefault();
+  
+    const id = parseInt(location.pathname.substring(10));
+  
+    const response = await fetch(`/api/blogs/${id}`, { method: "DELETE" });
+  
+    if (response.ok) {
+      window.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  };
+  document.getElementById("deleteBtn").addEventListener("click", deleteHandler)
