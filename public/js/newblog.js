05c2831f00505033ddcce1f5e7a85ef422fbff53
dExIT <dexit@dyc.lv>
2023-07-10 17:33:56 +0100

    Added js funcitonality to create blog post  cmment

---
+++ public/js/newblog.js
@@ -0,0 +1,24 @@
+
+const blogHandler = async (event) => {
+    event.preventDefault();
+
+
+    const title = document.querySelector('#new-blog-title').value;
+    const content = document.querySelector('#new-blog-content').value;
+    
+    const response = await fetch('/api/blogs', {
+        method: "POST",
+        body: JSON.stringify({ title, content }),   
+        headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+        window.location.replace('/dashboard')
+    } else {
+        alert(response.statusText);
+    }
+}
+
+const elementBlog = document.getElementById('new-blog-btn')
+if(elementBlog){
+    elementBlog.addEventListener('click', blogHandler);
+}
--- public/js/register.js
+++ public/js/register.js
@@ -1,34 +1,26 @@
-const regHandler = async event => {
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
+const signupHandler = async (event) => {
+  event.preventDefault();
+
+
+  const first_name = document.querySelector('#first_name').value.trim();
+  const last_name = document.querySelector('#last_name').value.trim();
+  const email = document.querySelector('#email').value.trim();
+  const password = document.querySelector('#password').value.trim();
   
-    // Gather the data from the form elements on the page
-    const email = document.querySelector("#email").value.trim();
-    const password = document.querySelector("#password").value.trim();
-    const first_name = document.querySelector("#first_name").value.trim();
-    const last_name = document.querySelector("#last_name").value.trim();
-    if (email && password) {
-      // Send the e-mail and password to the server
-      // post to /login
-  
-      // Send the e-mail and password to the server
-      // post to /login qithout fetch
-  
-      const response = await fetch("/api/users/register", {
-        method: "POST",
-        body: JSON.stringify({  first_name, last_name, email, password }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        document.location.replace("/");
-      } else {
-        alert("Failed to log in");
-      }
-    }
-  };
-  
-  document
-    .querySelector(".signupForm")
-    .addEventListener("submit", regHandler);
-  
+  const response = await fetch('/api/user', {
+      method: "POST",
+      body: JSON.stringify({ email, first_name, last_name, password}),   
+      headers: { "Content-Type": "application/json" },
+  });
+  if (response.ok) {
+      document.location.reload();
+  } else {
+      alert(response.statusText);
+  }
+}
+
+
+const elementRegister = document.getElementById('register')
+if(elementRegister){
+  elementRegister.addEventListener('click', signupHandler);
+}
