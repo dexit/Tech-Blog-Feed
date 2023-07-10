7b53fe1826743c7e5de2bbf8b2fd0a05e52cf2a9
dExIT <dexit@dyc.lv>
2023-07-10 17:04:00 +0100

    updated seeds and added the comments loops

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,20 +1,31 @@
 const sequelize = require("../config/connection");
-const { User, Blog } = require("../models").default;
-const users = require("./userData.json");
-const blogs = require("./blogData.json");
+const { User, Blog, Comment } = require("../models");
+
+const users = require("./users.json");
+const blogs = require("./blogs.json");
+const comments = require("./comments.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  const users = await User.bulkCreate(userData, {
+  const users = await User.bulkCreate(users, {
     individualHooks: true,
     returning: true,
   });
-  for (const blog of blogData) {
+  for (var blog of blogs) {
     await Blog.create({
       ...blog,
-      user_id: users[math.floor(Math.random() * users.length)],
+      user_id: users[Math.floor(Math.random() * users.length)].id
     });
   }
+    for (const comment of comments) {
+      await Comment.create({
+      ...comment,
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        blog_id: users[Math.floor(Math.random() * users.length)].id,
+      });
+      
+    }
+    
   process.exit(0);
 };
 seedDatabase();
