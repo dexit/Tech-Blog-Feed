6fa997482972cabf6f957fdf5fad399394e11e12
dExIT <dexit@dyc.lv>
2023-07-10 22:16:27 +0100

    fixed helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,11 @@
 module.exports = {
     format_date: (date) => {
       return date.toLocaleDateString();
+    },
+    excerpt: (text) => {
+        if (text.length > 20) {
+            text = text.substring(0,20) + "...";
+        }
+       return text;
     }
 }
