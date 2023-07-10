8ccb3f2039defdc4a79da897c3df79665ff31ed5
dExIT <dexit@dyc.lv>
2023-07-10 19:01:17 +0100

    need to re-do the whole thing

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,7 @@
-/* This code is creating a router object using the Express.js Router() method and then defining two
-routes: one for the "/api" endpoint and one for the root endpoint ("/"). It then uses these routes
-by calling the router.use() method and passing in the corresponding route and endpoint. Finally, it
-exports the router object so that it can be used in other parts of the application. */
 const router = require("express").Router();
-
 const apiRoutes = require("./api");
-const homeRoutes = require("./home-routes");
+const homeRoutes = require("./homeRoutes");
+
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 
