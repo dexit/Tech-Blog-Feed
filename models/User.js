36cb3db253e0b9ce4399a0dc7bfa2a31b062c2f8
dExIT <dexit@dyc.lv>
2023-07-10 16:53:31 +0100

    Fixed connection setup and the Users mdoel

--- models/User.js
+++ models/User.js
@@ -3,63 +3,42 @@ const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
-class User extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
-}
+
+class User extends Model {}
+
 User.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: true,
+      allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
+    username: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
     first_name: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
     },
     last_name: {
       type: DataTypes.STRING,
-      allowNull: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: true,
+      allowNull: false,
       validate: {
-        len: [2],
+        len: [8],
       },
     },
   },
   {
-    hooks: {
-      beforeCreate: async newUserData => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async updatedUserData => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
-    },
     sequelize,
-    timestamps: false,
     freezeTableName: true,
-    underscored: true,
     modelName: "user",
   }
 );
-
-module.exports = User;
+module.export = User;
