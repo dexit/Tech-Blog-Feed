918c9d841abe0b2c39c8c04e661f626c9e1b2448
dExIT <dexit@dyc.lv>
2023-07-10 22:24:16 +0100

    added excerpt helper util

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,9 +2,9 @@ module.exports = {
     format_date: (date) => {
       return date.toLocaleDateString();
     },
-    excerpt: (text) => {
+    excerpt: (text, blodid) => {
         if (text.length > 100) {
-            text = text.substring(0,100) + "...";
+            text = text.substring(0,100) + `... <a href="/homepage/${blodid}">read more</a>`;
         }
        return text;
     }
