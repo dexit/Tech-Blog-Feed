69cd8a4b6bd2761399e25fa4c50fb8ea642c090d
dExIT <dexit@dyc.lv>
2023-07-10 16:54:39 +0100

    added seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,1 +1,20 @@
-//seeds go here
+const sequelize = require("../config/connection");
+const { User, Blog } = require("../models").default;
+const users = require("./userData.json");
+const blogs = require("./blogData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[math.floor(Math.random() * users.length)],
+    });
+  }
+  process.exit(0);
+};
+seedDatabase();
