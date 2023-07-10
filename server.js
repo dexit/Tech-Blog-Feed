08ab1d48e0c8d797d7f17b4d67841e7e34dc0488
dExIT <dexit@dyc.lv>
2023-07-10 18:15:55 +0100

    fixing theissue with heroku now serverjs

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
+/* These lines of code are importing necessary modules and dependencies for the application. */
 const express = require("express");
 const path = require("path");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const helpers = require("./utils/helpers");
-const routes = require("./controllers");
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const helpers = require('./utils/helpers')
 
+const routes = require("./controllers");
 // const helpers = require("./util/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({helpers});
+/* `const hbs = exphbs.create({ helpers });` creates a new instance of the Handlebars engine with the
+`helpers` object passed as an argument. This allows the application to use custom helper functions
+in the Handlebars templates. */
+const hbs = exphbs.create({ helpers });
 const sess = {
   secret: "Super secret secret",
-  cookie: {},
+  cookie: {maxAge: 300000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
 };
+/* These lines of code are setting up and configuring the Express application. */
 app.use(session(sess));
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.text());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -37,5 +37,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening http://localhost:3001"));
+  app.listen(PORT, () => console.log("Now listening"));
 });
